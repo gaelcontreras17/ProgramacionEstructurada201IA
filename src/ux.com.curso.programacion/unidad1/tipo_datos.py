72cd9ebcb9e92b1e483f0a47c52773b24f924f9a
gael <gaelcv07@gmail.com>
2026-03-10 08:39:42 -0600

    Tipos de datos

---
+++ src/ux.com.curso.programacion/unidad1/tipo_datos.py
@@ -0,0 +1,26 @@
+#tipo de datos en python
+def datos():
+
+    #Demostracion de tipo de datos en python
+
+    entero =25
+    decimal = 3.14
+    cadena = "Hola, Mundo!"
+    booleano =True
+    
+    print(entero)
+    print(decimal)
+    print(cadena)
+    print(booleano)
+
+def tipos_datos_compuestos():
+    lista=[10,20,30,40]
+    tupla=(19,29,39,49)
+    diccionario ={"Nombre": "gael", "edad": 30, "ciudad": "Cancun"}
+
+def main():
+    datos()
+    tipos_datos_compuestos()
+
+if __name__ == "__main__":
+    main()
