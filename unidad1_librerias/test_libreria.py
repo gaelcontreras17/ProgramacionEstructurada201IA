ec4f7dad01b2c131e63f0165c6a823d8310ff5c7
gaelcontreras17 <ux25ii296@ux.edu.mx>
2026-03-17 14:56:24 -0600

    Create test_libreria.py

---
+++ unidad1_librerias/test_libreria.py
@@ -0,0 +1,9 @@
+import pyfiglet
+import sys
+
+banner = pyfiglet.figlet_format("Estructurada")
+print(banner)
+
+print(f"Version de python: {sys.version}")
+print("-" * 30)
+print("¡Libreria Instalada correctamente en el entorno virtual!")
