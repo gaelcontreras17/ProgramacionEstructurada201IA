a5b5b110b1bfd90698e36bf5ee460d59b94b9563
gael <gaelcv07@gmail.com>
2026-05-26 08:14:37 -0600

    feat: agregar ejercicio if-else

---
+++ src/ux.com.curso.programacion/unidad5/calcular.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+
+def calcular(num1, num2):
+    suma = num1 + num2
+    resta = num1 - num2
+    producto = num1 * num2
+    print(suma)
+    print(resta)
+    print(producto)
+
+calcular(a, b)
